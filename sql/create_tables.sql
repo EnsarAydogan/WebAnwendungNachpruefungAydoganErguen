99b2e04d44e3fdf6522262283dc6bcfa045cbc27
EnsarAydogan <ensoenso10@gmail.com>
2023-10-15 13:04:19 +0200

    sqlchanges

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,16 +1,29 @@
+CREATE TABLE user (
+	userid INTEGER PRIMARY KEY AUTOINCREMENT,
+	username TEXT NOT NULL,
+	password TEXT NOT NULL
+);
+
 CREATE TABLE todo (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	complete BOOLEAN DEFAULT FALSE,
-	description TEXT NOT NULL
+	description TEXT NOT NULL,
+	user_userid INTEGER,
+	FOREIGN KEY (user_userid) REFERENCES user (userid) ON DELETE CASCADE
 );
+
 CREATE TABLE list (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL
+	user_userid INTEGER,
+	FOREIGN KEY (user_userid) REFERENCES user (userid) ON DELETE CASCADE
 );
 CREATE TABLE todo_list (
 	todo_id INTEGER,
 	list_id INTEGER,
 	PRIMARY KEY (todo_id, list_id),
+	user_userid INTEGER,
 	FOREIGN KEY (todo_id) REFERENCES todo (id) ON DELETE CASCADE,
-	FOREIGN KEY (list_id) REFERENCES list (id) ON DELETE CASCADE
+	FOREIGN KEY (list_id) REFERENCES list (id) ON DELETE CASCADE,
+	FOREIGN KEY (user_userid) REFERENCES user (userid) ON DELETE CASCADE
 );
